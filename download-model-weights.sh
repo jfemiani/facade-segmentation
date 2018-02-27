e00372fc18fa7c38e6698533f2b45927f26e2f19
John <femianjc@miamioh.edu>
2018-02-27 15:46:08 +0000

    Added a nb for qualitative evaluation of the 'feedback' net

--- download-model-weights.sh
+++ download-model-weights.sh
@@ -2,13 +2,19 @@
 
 echo "Downloading Caffe Models"
 
+# I put all of the files into one 'curl' command so that we ger 
+# a unified progress message. 
+# The `-C -` option prevents us from downloading files that
+# we already have locally. 
 curl -C - "http://handouts1.csi.miamioh.edu:82/vision/facades/{i12-weights-caffe,crf-deploy,crf-initial-weights-caffe,low-rank-weights-caffe,segnet5-weights-caffe,segnet_driving}.zip" -o "data/#1.zip"
 
 echo "Extracting Caffe Models"
 
-unzip data/i12-weights-caffe.zip
-unzip data/crf-deploy.zip
-unzip data/crf-initial-weights-caffe.zip
-unzip data/low-rank-weights-caffe.zip
-unzip data/segnet5-weights-caffe.zip
-unzip data/segnet_driving.zip
+# The -n flag is used to prevent us from modifing files that 
+# already exist UNLESS the one in the zip file is newer. 
+unzip -n data/i12-weights-caffe.zip
+unzip -n data/crf-deploy.zip
+unzip -n data/crf-initial-weights-caffe.zip
+unzip -n data/low-rank-weights-caffe.zip
+unzip -n data/segnet5-weights-caffe.zip
+unzip -n data/segnet_driving.zip
